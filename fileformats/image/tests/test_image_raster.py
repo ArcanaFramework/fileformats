a77c672f87db4d90cb4e99e4a050955f9d6d8008
Tom Close <tom.g.close@gmail.com>
2023-01-23 14:09:10 +1030

    added more converter tests for image

---
+++ fileformats/image/tests/test_image_raster.py
@@ -0,0 +1,24 @@
+import shutil
+import pytest
+from imageio.core.fetching import get_remote_file
+from fileformats.image import Png, Tiff
+from fileformats.core.exceptions import FormatMismatchError
+
+
+@pytest.fixture(scope="session")
+def png_path():
+    return get_remote_file("images/chelsea.png")
+
+
+def test_tiff_endianness(png_path):
+    png = Png(png_path)
+    tiff = Tiff.convert(png)
+    tiff.validate()
+
+
+def test_tiff_fail(png_path, work_dir):
+    bad_tiff_path = work_dir / "bad_tiff.tiff"
+    shutil.copyfile(png_path, bad_tiff_path)
+    bad_tiff = Tiff(bad_tiff_path)
+    with pytest.raises(FormatMismatchError):
+        bad_tiff.validate()
