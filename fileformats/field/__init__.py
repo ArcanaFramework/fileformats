5e9d7d365e48a0a94a188e40f8d69d15eaa9f79a
Tom Close <tom.g.close@gmail.com>
2023-01-24 15:35:05 +1030

    implemented field types

--- fileformats/field/__init__.py
+++ fileformats/field/__init__.py
@@ -1,33 +1,143 @@
+import attrs
 from ..core import DataType
+from ..core.exceptions import FormatMismatchError
 
 
+@attrs.define
 class Field(DataType):
+
+    value = attrs.field()
+
+    def __str__(self):
+        return str(self.value)
+
+
+@attrs.define
+class SingleField(Field):
     pass
 
 
-class Text(Field):
-    pass
+class Scalar:
+    def __add__(self, other):
+        return self.value + other
+
+    def __sub__(self, other):
+        return self.value - other
+
+    def __mul__(self, other):
+        return self.value * other
+
+    def __truediv__(self, other):
+        return self.value / other
+
+    def __floordiv__(self, other):
+        return self.value // other
+
+    def __mod__(self, other):
+        return self.value % other
+
+    def __pow__(self, other):
+        return self.value**other
+
+    def __neg__(self):
+        return -self.value
 
 
-class Integer(Field):
-    pass
+def boolean_converter(in_value):
+    if isinstance(in_value, str):
+        if in_value.lower() in ("true", "1", "yes"):
+            value = True
+        elif in_value.lower() in ("false", "0", "no"):
+            value = False
+        else:
+            raise FormatMismatchError(
+                f"Cannot convert string '{in_value}' to boolean value"
+            )
+    else:
+        value = bool(in_value)
+    return value
 
 
-class Decimal(Field):
-    pass
+def array_converter(in_value):
+    if in_value.startswith("[") and in_value.endswith("]"):
+        in_value = in_value[1:-1]
+    return in_value.split(",")
 
 
-class Boolean(Field):
-    pass
+@attrs.define
+class Text(SingleField):
+    value: str = attrs.field(converter=str)
 
 
+@attrs.define
+class Integer(SingleField, Scalar):
+    value: int = attrs.field(converter=float)
+
+    def __int__(self):
+        return self.value
+
+
+@attrs.define
+class Decimal(SingleField, Scalar):
+    value: float = attrs.field(converter=float)
+
+    def __float__(self):
+        return self.value
+
+
+@attrs.define
+class Boolean(SingleField):
+    value: bool = attrs.field(converter=float)
+
+    def __str__(self):
+        return str(self.value).lower()
+
+    def __bool__(self):
+        return self.value
+
+    def __and__(self, other):
+        return self.value and other.value
+
+    def __or__(self, other):
+        return self.value or other.value
+
+    def __not__(self):
+        return not self.value
+
+    def __invert__(self):
+        return not self.value
+
+
+@attrs.define
 class Array(Field):
+
+    value: list = attrs.field(converter=array_converter)
 
     item_type = None
 
+    def __attrs_post_init__(self):
+        # Ensure items are of the correct type
+        if self.item_type is not None:
+            self.value = [self.item_type(i).value for i in self.value]
+
+    def __str__(self):
+        return (
+            "["
+            + ",".join(
+                str(self.item_type(i)) if self.item_type is not None else i
+                for i in self.value
+            )
+            + "]"
+        )
+
     @classmethod
     def __class_getitem__(cls, item_type):
-        """Set the content types for a newly created dynamically type"""
+        """Set the item type for a newly created dynamically type"""
+        if not isinstance(item_type, SingleField):
+            raise RuntimeError(
+                "Can only provide SingleField type as item types for Array fields, not "
+                f"{item_type}"
+            )
         return type(
             f"{item_type.__name__}_{cls.__name__}",
             (cls,),
