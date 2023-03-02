60d98f29ad658bb387d6ce4e8038f0ad1aa2b1e5
Tom Close <tom.g.close@gmail.com>
2023-03-02 18:33:16 +1100

    renamed BaseDirectory back to Directory and created TypedSet, DirectoryContaining and SetOf generic classes

--- fileformats/generic/__init__.py
+++ fileformats/generic/__init__.py
@@ -122,7 +122,7 @@ class File(FsObject):
 
 
 @attrs.define
-class BaseDirectory(FsObject):
+class Directory(FsObject):
     """Base directory to be overridden by subtypes that represent directories but don't
     want to inherit content type "qualifers" (i.e. most of them)"""
 
@@ -192,10 +192,51 @@ class BaseDirectory(FsObject):
         return super().hash_files(relative_to=relative_to, **kwargs)
 
 
-class Directory(WithQualifiers, BaseDirectory):
-    """Generic directory, which can be qualified by the formats of its contents"""
+class TypedSet(FileSet):
+    """List of specific file types (similar to the contents of a directory but not
+    enclosed in one)"""
+
+    content_types = ()
+
+    @property
+    def contents(self):
+        for content_type in self.content_types:
+            for p in self.fspaths:
+                try:
+                    yield content_type([p])
+                except FormatMismatchError:
+                    continue
+
+    @mark.check
+    def validate_contents(self):
+        if not self.content_types:
+            return
+        not_found = set(self.content_types)
+        for fspath in self.fspaths:
+            for content_type in list(not_found):
+                if content_type.matches(fspath):
+                    not_found.remove(content_type)
+                    if not not_found:
+                        return
+        assert not_found
+        raise FormatMismatchError(
+            f"Did not find the required content types, {not_found}, within the "
+            f"given list {self.fspaths}"
+        )
+
+
+class DirectoryContaining(WithQualifiers, Directory):
+    """Generic directory qualified by the formats of its contents"""
 
     # WithQualifiers-required class attrs
     qualifiers_attr_name = "content_types"
     allowed_qualifiers = (FileSet,)
-    generically_qualified = True
+    generically_qualifies = True
+
+
+class SetOf(WithQualifiers, TypedSet):
+
+    # WithQualifiers-required class attrs
+    qualifiers_attr_name = "content_types"
+    allowed_qualifiers = (FileSet,)
+    generically_qualifies = True
