d7126af72b06ac28cd4d59cf563cf6c50ee6f8ed
Tom Close <tom.g.close@gmail.com>
2023-06-14 12:20:33 +1000

    reworked copy() method to use CopyMode enum

---
+++ fileformats/core/field.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+import typing as ty
+import attrs
+from .utils import (
+    classproperty,
+)
+from .exceptions import (
+    FileFormatsError,
+)
+from .datatype import DataType
+
+
+@attrs.define
+class Field(DataType):
+    value = attrs.field()
+
+    type = None
+    is_field = True
+    primitive = None
+
+    def __str__(self):
+        return str(self.value)
+
+    @property
+    def metadata(self):
+        return {}
+
+    @classproperty
+    def all_fields(cls) -> list[ty.Type[Field]]:  # pylint: disable=no-self-argument
+        """Iterate over all field formats in fileformats.* namespaces"""
+        import fileformats.field  # noqa
+
+        return [f for f in Field.subclasses() if f.primitive is not None]
+
+    @classmethod
+    def from_primitive(cls, dtype: type):
+        try:
+            datatype = next(iter(f for f in cls.all_fields if f.primitive is dtype))
+        except StopIteration as e:
+            field_types_str = ", ".join(t.__name__ for t in cls.all_fields)
+            raise FileFormatsError(
+                f"{dtype} doesn't not correspond to a valid fileformats field type "
+                f"({field_types_str})"
+            ) from e
+        return datatype
+
+    _all_fields = None
--- fileformats/core/tests/test_utils.py
+++ fileformats/core/tests/test_utils.py
@@ -9,7 +9,6 @@ from fileformats.core.mixin import WithSeparateHeader
 from fileformats.core.utils import (
     find_matching,
     MissingExtendedDependency,
-    CifsMountIndentifier,
 )
 from fileformats.core.exceptions import MissingExtendedDepenciesError
 import fileformats.text
@@ -74,31 +73,31 @@ def test_copy(fsobject: FsObject, dest_dir: Path):
 
 
 def test_copy_symlink(fsobject: FsObject, dest_dir: Path):
-    cpy = fsobject.copy(dest_dir, link_type="symbolic")
+    cpy = fsobject.copy(dest_dir, mode=File.CopyMode.symlink)
     assert all(p.parent == dest_dir for p in cpy.fspaths)
     assert set(p.name for p in cpy.fspaths) == set(p.name for p in fsobject.fspaths)
     assert all(p.is_symlink() for p in cpy.fspaths)
     assert cpy.hash() == fsobject.hash()
 
 
-def test_copy_symlink_with_cifs_fallback_on_cifs(
-    fsobject: FsObject, dest_dir: Path, work_dir: Path
-):
+def test_copy_symlink_with_fallback(fsobject: FsObject, dest_dir: Path):
     "Simulate source object on CIFS share, should be copied not symlinked"
-    with CifsMountIndentifier.patch_table([(str(work_dir), True)]):
-        cpy = fsobject.copy(dest_dir, link_type="symbolic_with_cifs_fallback")
+    cpy = fsobject.copy(
+        dest_dir,
+        mode=File.CopyMode.link_or_copy,
+        supported_modes=File.CopyMode.hardlink_or_copy,
+    )
     assert all(p.parent == dest_dir for p in cpy.fspaths)
     assert set(p.name for p in cpy.fspaths) == set(p.name for p in fsobject.fspaths)
     assert not any(p.is_symlink() for p in cpy.fspaths)
     assert cpy.hash() == fsobject.hash()
 
 
-def test_copy_symlink_with_cifs_fallback_not_on_cifs(
-    fsobject: FsObject, dest_dir: Path, work_dir: Path
-):
+def test_copy_symlink_with_unrequired_fallback(fsobject: FsObject, dest_dir: Path):
     "Simulate source object not on CIFS share, should be symlinked not copied"
-    with CifsMountIndentifier.patch_table([(str(work_dir), False)]):
-        cpy = fsobject.copy(dest_dir, link_type="symbolic_with_cifs_fallback")
+    cpy = fsobject.copy(
+        dest_dir, mode=File.CopyMode.link_or_copy, supported_modes=File.CopyMode.link
+    )
     assert all(p.parent == dest_dir for p in cpy.fspaths)
     assert set(p.name for p in cpy.fspaths) == set(p.name for p in fsobject.fspaths)
     assert all(p.is_symlink() for p in cpy.fspaths)
@@ -106,7 +105,7 @@ def test_copy_symlink_with_cifs_fallback_not_on_cifs(
 
 
 def test_copy_hardlink(fsobject: FsObject, dest_dir: Path):
-    cpy = fsobject.copy(dest_dir, link_type="hard")
+    cpy = fsobject.copy(dest_dir, mode=File.CopyMode.symlink)
     assert all(p.parent == dest_dir for p in cpy.fspaths)
     assert set(p.name for p in cpy.fspaths) == set(p.name for p in fsobject.fspaths)
     assert all(
@@ -234,185 +233,3 @@ def test_hash_files(fsobject: FsObject, work_dir: Path, dest_dir: Path):
     )
     cpy = fsobject.copy(dest_dir)
     assert cpy.hash_files() == fsobject.hash_files()
-
-
-MOUNT_OUTPUTS = (
-    # Linux, no CIFS
-    (
-        r"""sysfs on /sys type sysfs (rw,nosuid,nodev,noexec,relatime)
-proc on /proc type proc (rw,nosuid,nodev,noexec,relatime)
-udev on /dev type devtmpfs (rw,nosuid,relatime,size=8121732k,nr_inodes=2030433,mode=755)
-devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000)
-tmpfs on /run type tmpfs (rw,nosuid,noexec,relatime,size=1628440k,mode=755)
-/dev/nvme0n1p2 on / type ext4 (rw,relatime,errors=remount-ro,data=ordered)
-securityfs on /sys/kernel/security type securityfs (rw,nosuid,nodev,noexec,relatime)
-tmpfs on /dev/shm type tmpfs (rw,nosuid,nodev)
-tmpfs on /sys/fs/cgroup type tmpfs (ro,nosuid,nodev,noexec,mode=755)
-cgroup on /sys/fs/cgroup/systemd type cgroup (rw,nosuid,nodev,noexec,relatime,xattr,release_agent=/lib/systemd/systemd-cgroups-agent,name=systemd)
-pstore on /sys/fs/pstore type pstore (rw,nosuid,nodev,noexec,relatime)
-efivarfs on /sys/firmware/efi/efivars type efivarfs (rw,nosuid,nodev,noexec,relatime)
-cgroup on /sys/fs/cgroup/cpu,cpuacct type cgroup (rw,nosuid,nodev,noexec,relatime,cpu,cpuacct)
-cgroup on /sys/fs/cgroup/freezer type cgroup (rw,nosuid,nodev,noexec,relatime,freezer)
-cgroup on /sys/fs/cgroup/pids type cgroup (rw,nosuid,nodev,noexec,relatime,pids)
-cgroup on /sys/fs/cgroup/cpuset type cgroup (rw,nosuid,nodev,noexec,relatime,cpuset)
-systemd-1 on /proc/sys/fs/binfmt_misc type autofs (rw,relatime,fd=26,pgrp=1,timeout=0,minproto=5,maxproto=5,direct)
-hugetlbfs on /dev/hugepages type hugetlbfs (rw,relatime)
-debugfs on /sys/kernel/debug type debugfs (rw,relatime)
-mqueue on /dev/mqueue type mqueue (rw,relatime)
-fusectl on /sys/fs/fuse/connections type fusectl (rw,relatime)
-/dev/nvme0n1p1 on /boot/efi type vfat (rw,relatime,fmask=0077,dmask=0077,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro)
-/dev/nvme0n1p2 on /var/lib/docker/aufs type ext4 (rw,relatime,errors=remount-ro,data=ordered)
-gvfsd-fuse on /run/user/1002/gvfs type fuse.gvfsd-fuse (rw,nosuid,nodev,relatime,user_id=1002,group_id=1002)
-""",
-        0,
-        [],
-    ),
-    # OS X, no CIFS
-    (
-        r"""/dev/disk2 on / (hfs, local, journaled)
-devfs on /dev (devfs, local, nobrowse)
-map -hosts on /net (autofs, nosuid, automounted, nobrowse)
-map auto_home on /home (autofs, automounted, nobrowse)
-map -fstab on /Network/Servers (autofs, automounted, nobrowse)
-/dev/disk3s2 on /Volumes/MyBookData (hfs, local, nodev, nosuid, journaled)
-afni:/elrond0 on /Volumes/afni (nfs)
-afni:/var/www/INCOMING on /Volumes/INCOMING (nfs)
-afni:/fraid on /Volumes/afni (nfs, asynchronous)
-boromir:/raid.bot on /Volumes/raid.bot (nfs)
-elros:/volume2/AFNI_SHARE on /Volumes/AFNI_SHARE (nfs)
-map -static on /Volumes/safni (autofs, automounted, nobrowse)
-map -static on /Volumes/raid.top (autofs, automounted, nobrowse)
-/dev/disk1s3 on /Volumes/Boot OS X (hfs, local, journaled, nobrowse)
-""",
-        0,
-        [],
-    ),
-    # Non-zero exit code
-    ("", 1, []),
-    # Variant of Linux example with CIFS added manually
-    (
-        r"""sysfs on /sys type sysfs (rw,nosuid,nodev,noexec,relatime)
-proc on /proc type proc (rw,nosuid,nodev,noexec,relatime)
-udev on /dev type devtmpfs (rw,nosuid,relatime,size=8121732k,nr_inodes=2030433,mode=755)
-devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=000)
-tmpfs on /run type tmpfs (rw,nosuid,noexec,relatime,size=1628440k,mode=755)
-/dev/nvme0n1p2 on / type ext4 (rw,relatime,errors=remount-ro,data=ordered)
-securityfs on /sys/kernel/security type securityfs (rw,nosuid,nodev,noexec,relatime)
-tmpfs on /dev/shm type tmpfs (rw,nosuid,nodev)
-tmpfs on /sys/fs/cgroup type tmpfs (ro,nosuid,nodev,noexec,mode=755)
-cgroup on /sys/fs/cgroup/systemd type cgroup (rw,nosuid,nodev,noexec,relatime,xattr,release_agent=/lib/systemd/systemd-cgroups-agent,name=systemd)
-pstore on /sys/fs/pstore type pstore (rw,nosuid,nodev,noexec,relatime)
-efivarfs on /sys/firmware/efi/efivars type efivarfs (rw,nosuid,nodev,noexec,relatime)
-cgroup on /sys/fs/cgroup/cpu,cpuacct type cgroup (rw,nosuid,nodev,noexec,relatime,cpu,cpuacct)
-cgroup on /sys/fs/cgroup/freezer type cgroup (rw,nosuid,nodev,noexec,relatime,freezer)
-cgroup on /sys/fs/cgroup/pids type cgroup (rw,nosuid,nodev,noexec,relatime,pids)
-cgroup on /sys/fs/cgroup/cpuset type cgroup (rw,nosuid,nodev,noexec,relatime,cpuset)
-systemd-1 on /proc/sys/fs/binfmt_misc type autofs (rw,relatime,fd=26,pgrp=1,timeout=0,minproto=5,maxproto=5,direct)
-hugetlbfs on /dev/hugepages type hugetlbfs (rw,relatime)
-debugfs on /sys/kernel/debug type debugfs (rw,relatime)
-mqueue on /dev/mqueue type mqueue (rw,relatime)
-fusectl on /sys/fs/fuse/connections type fusectl (rw,relatime)
-/dev/nvme0n1p1 on /boot/efi type vfat (rw,relatime,fmask=0077,dmask=0077,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro)
-/dev/nvme0n1p2 on /var/lib/docker/aufs type ext4 (rw,relatime,errors=remount-ro,data=ordered)
-gvfsd-fuse on /run/user/1002/gvfs type fuse.gvfsd-fuse (rw,nosuid,nodev,relatime,user_id=1002,group_id=1002)
-""",
-        0,
-        [],
-    ),
-    # Variant of OS X example with CIFS added manually
-    (
-        r"""/dev/disk2 on / (hfs, local, journaled)
-devfs on /dev (devfs, local, nobrowse)
-afni:/elrond0 on /Volumes/afni (cifs)
-afni:/var/www/INCOMING on /Volumes/INCOMING (nfs)
-afni:/fraid on /Volumes/afni/fraid (nfs, asynchronous)
-boromir:/raid.bot on /Volumes/raid.bot (nfs)
-elros:/volume2/AFNI_SHARE on /Volumes/AFNI_SHARE (nfs)
-""",
-        0,
-        [("/Volumes/afni/fraid", "nfs"), ("/Volumes/afni", "cifs")],
-    ),
-    # From Windows: docker run --rm -it -v C:\:/data busybox mount
-    (
-        r"""overlay on / type overlay (rw,relatime,lowerdir=/var/lib/docker/overlay2/l/26UTYITLF24YE7KEGTMHUNHPPG:/var/lib/docker/overlay2/l/SWGNP3T2EEB4CNBJFN3SDZLXHP,upperdir=/var/lib/docker/overlay2/a4c54ab1aa031bb5a14a424abd655510521e183ee4fa4158672e8376c89df394/diff,workdir=/var/lib/docker/overlay2/a4c54ab1aa031bb5a14a424abd655510521e183ee4fa4158672e8376c89df394/work)
-proc on /proc type proc (rw,nosuid,nodev,noexec,relatime)
-tmpfs on /dev type tmpfs (rw,nosuid,size=65536k,mode=755)
-devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=666)
-sysfs on /sys type sysfs (ro,nosuid,nodev,noexec,relatime)
-tmpfs on /sys/fs/cgroup type tmpfs (ro,nosuid,nodev,noexec,relatime,mode=755)
-cpuset on /sys/fs/cgroup/cpuset type cgroup (ro,nosuid,nodev,noexec,relatime,cpuset)
-cpu on /sys/fs/cgroup/cpu type cgroup (ro,nosuid,nodev,noexec,relatime,cpu)
-cpuacct on /sys/fs/cgroup/cpuacct type cgroup (ro,nosuid,nodev,noexec,relatime,cpuacct)
-blkio on /sys/fs/cgroup/blkio type cgroup (ro,nosuid,nodev,noexec,relatime,blkio)
-memory on /sys/fs/cgroup/memory type cgroup (ro,nosuid,nodev,noexec,relatime,memory)
-devices on /sys/fs/cgroup/devices type cgroup (ro,nosuid,nodev,noexec,relatime,devices)
-freezer on /sys/fs/cgroup/freezer type cgroup (ro,nosuid,nodev,noexec,relatime,freezer)
-net_cls on /sys/fs/cgroup/net_cls type cgroup (ro,nosuid,nodev,noexec,relatime,net_cls)
-perf_event on /sys/fs/cgroup/perf_event type cgroup (ro,nosuid,nodev,noexec,relatime,perf_event)
-net_prio on /sys/fs/cgroup/net_prio type cgroup (ro,nosuid,nodev,noexec,relatime,net_prio)
-hugetlb on /sys/fs/cgroup/hugetlb type cgroup (ro,nosuid,nodev,noexec,relatime,hugetlb)
-pids on /sys/fs/cgroup/pids type cgroup (ro,nosuid,nodev,noexec,relatime,pids)
-cgroup on /sys/fs/cgroup/systemd type cgroup (ro,nosuid,nodev,noexec,relatime,name=systemd)
-mqueue on /dev/mqueue type mqueue (rw,nosuid,nodev,noexec,relatime)
-//10.0.75.1/C on /data type cifs (rw,relatime,vers=3.02,sec=ntlmsspi,cache=strict,username=filo,domain=MSI,uid=0,noforceuid,gid=0,noforcegid,addr=10.0.75.1,file_mode=0755,dir_mode=0755,iocharset=utf8,nounix,serverino,mapposix,nobrl,mfsymlinks,noperm,rsize=1048576,wsize=1048576,echo_interval=60,actimeo=1)
-/dev/sda1 on /etc/resolv.conf type ext4 (rw,relatime,data=ordered)
-/dev/sda1 on /etc/hostname type ext4 (rw,relatime,data=ordered)
-/dev/sda1 on /etc/hosts type ext4 (rw,relatime,data=ordered)
-shm on /dev/shm type tmpfs (rw,nosuid,nodev,noexec,relatime,size=65536k)
-devpts on /dev/console type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=666)
-proc on /proc/bus type proc (ro,relatime)
-proc on /proc/fs type proc (ro,relatime)
-proc on /proc/irq type proc (ro,relatime)
-proc on /proc/sys type proc (ro,relatime)
-proc on /proc/sysrq-trigger type proc (ro,relatime)
-tmpfs on /proc/kcore type tmpfs (rw,nosuid,size=65536k,mode=755)
-tmpfs on /proc/timer_list type tmpfs (rw,nosuid,size=65536k,mode=755)
-tmpfs on /proc/sched_debug type tmpfs (rw,nosuid,size=65536k,mode=755)
-tmpfs on /proc/scsi type tmpfs (ro,relatime)
-tmpfs on /sys/firmware type tmpfs (ro,relatime)
-""",
-        0,
-        [("/data", "cifs")],
-    ),
-    # From @yarikoptic - added blank lines to test for resilience
-    (
-        r"""/proc on /proc type proc (rw,relatime)
-sysfs on /sys type sysfs (rw,nosuid,nodev,noexec,relatime)
-tmpfs on /dev/shm type tmpfs (rw,relatime)
-devpts on /dev/pts type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=666)
-
-devpts on /dev/ptmx type devpts (rw,nosuid,noexec,relatime,gid=5,mode=620,ptmxmode=666)
-
-""",
-        0,
-        [],
-    ),
-)
-
-
-@pytest.mark.parametrize("output, exit_code, expected", MOUNT_OUTPUTS)
-def test_parse_mount_table(output, exit_code, expected):
-    assert CifsMountIndentifier.parse_mount_table(exit_code, output) == expected
-
-
-def test_cifs_check():
-    assert isinstance(CifsMountIndentifier.mount_table, list)
-    assert isinstance(CifsMountIndentifier.on_cifs("/"), bool)
-    fake_table = [("/scratch/tmp", False), ("/scratch", True)]
-    cifs_targets = [
-        ("/scratch/tmp/x/y", False),
-        ("/scratch/tmp/x", False),
-        ("/scratch/x/y", True),
-        ("/scratch/x", True),
-        ("/x/y", False),
-        ("/x", False),
-        ("/", False),
-    ]
-
-    with CifsMountIndentifier.patch_table([]):
-        for target, _ in cifs_targets:
-            assert CifsMountIndentifier.on_cifs(target) is False
-
-    with CifsMountIndentifier.patch_table(fake_table):
-        for target, expected in cifs_targets:
-            assert CifsMountIndentifier.on_cifs(target) is expected
