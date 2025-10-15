0b03e5a57a766b8a812c63b9f5e787c9727591b7
Thomas G. Close <tom.g.close@gmail.com>
2025-10-15 13:42:41 +1100

    debugging mime-type translations

--- extras/fileformats/extras/generic/tests/test_converters.py
+++ extras/fileformats/extras/generic/tests/test_converters.py
@@ -1,6 +1,8 @@
-import pytest
-from pathlib import Path
 import typing as ty
+from pathlib import Path
+
+import pytest
+
 from fileformats.generic import DirectoryOf, SetOf
 from fileformats.text import TextFile
 
@@ -24,15 +26,17 @@ def test_files(files_dir: Path, file_names: ty.List[str]) -> ty.List[Path]:
     return [files_dir / n for n in file_names]
 
 
-def test_list_dir_contents(files_dir: Path, test_files: ty.List[Path]) -> None:
-    text_set = SetOf[TextFile].convert(DirectoryOf[TextFile](files_dir))  # type: ignore[misc]
+def test_list_dir_contents(
+    files_dir: Path, test_files: ty.List[Path], tmp_path: Path
+) -> None:
+    text_set = SetOf[TextFile].convert(DirectoryOf[TextFile](files_dir), cache_root=tmp_path / "cache")  # type: ignore[misc]
     assert sorted(t.name for t in text_set.contents) == sorted(
         f.name for f in test_files
     )
 
 
 def test_put_contents_in_dir(
-    file_names: ty.List[str], test_files: ty.List[Path]
+    file_names: ty.List[str], test_files: ty.List[Path], tmp_path: Path
 ) -> None:
-    text_dir = DirectoryOf[TextFile].convert(SetOf[TextFile](test_files))  # type: ignore[misc]
+    text_dir = DirectoryOf[TextFile].convert(SetOf[TextFile](test_files), cache_root=tmp_path / "cache")  # type: ignore[misc]
     assert sorted(t.name for t in text_dir.contents) == file_names
